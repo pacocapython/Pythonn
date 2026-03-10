624b75134a47977e68511e9f98f6c06a21c95f13
pacocapython <gabrielgustavosv8@gmail.com>
2026-03-10 11:12:17 -0300

    Add files via upload

---
+++ python - equações lineares.py
@@ -0,0 +1,12 @@
+a =int(input("Digite o número: "))
+b = int(input("Digite o número: "))
+c = int(input("Digite o número: "))
+d = int(input("Digite o número: "))
+e = int(input("Digite o número: "))
+f = int(input("Digite o número: "))
+
+y = (a * f) - (c * d) / (a * e) - (b * d) 
+x = (c * e) - (b * f) / (a * e) - (b * d)
+
+print (f'os valores de xe y são {x} e {y}')
+#GABRIEL GUSTAVO SOUZA DE VARGAS
