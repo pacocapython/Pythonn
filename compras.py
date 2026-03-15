e10c3376d1933d64cd3f79f03c538e6a69852cb3
pacocapython <gabrielgustavosv8@gmail.com>
2026-03-15 16:59:37 -0300

    Add files via upload

---
+++ compras.py
@@ -0,0 +1,24 @@
+print("Bem vindo ao seu Programa de suas Compras")
+print("Programador: PaçocaPython")
+
+p1 = input("Digite o nome do primeiro produto: ")
+v1 = float(input("Digite o preço: "))
+
+p2 = input("Digite o nome do segundo produto: ")
+v2 = float(input("Digite o preço: "))
+
+p3 = input("Digite o nome do terceiro produto: ")
+v3 = float(input("Digite o preço: "))
+
+p4 = input("Digite o nome do quarto produto: ")
+v4 = float(input("Digite o preço: "))
+
+p5 = input("Digite o nome do quinto produto: ")
+v5 = float(input("Digite o preço: "))
+
+print("Produtos que você comprou")
+print(p1, "R$", v1)
+print(p2, "R$", v2)
+print(p3, "R$", v3)
+print(p4, "R$", v4)
+print(p5, "R$", v5)
